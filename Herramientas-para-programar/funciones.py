be6341254c61c6701cf55222c0b42a6c40bae96f
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-09-06 15:24:41 -0300

    Clase N°17: Modularizando nuestro conversor de monedas.

--- Herramientas-para-programar/funciones.py
+++ Herramientas-para-programar/funciones.py
@@ -7,18 +7,27 @@
 # imprimir_mensaje()
 # imprimir_mensaje()
 
-def conversacion(mensaje: str) -> None:
-    print('Hola.')
-    print('¿Cómo estás?')
-    print(f'Elegiste la opción {mensaje}.')
-    print('Adios.')
+# def conversacion(mensaje: str) -> None:
+#     print('Hola.')
+#     print('¿Cómo estás?')
+#     print(f'Elegiste la opción {mensaje}.')
+#     print('Adios.')
 
-opcion = input('Elige una opción (1, 2, 3): ')
-if opcion == '1':
-    conversacion('1')
-elif opcion == '2':
-    conversacion('2')
-elif opcion == '3':
-    conversacion('3')
-else:
-    print('Ingresa una opción correcta, por favor.')
+# opcion = input('Elige una opción (1, 2, 3): ')
+# if opcion == '1':
+#     conversacion('1')
+# elif opcion == '2':
+#     conversacion('2')
+# elif opcion == '3':
+#     conversacion('3')
+# else:
+#     print('Ingresa una opción correcta, por favor.')
+
+
+def suma(a: int, b: int):
+    print('Se suman dos números')
+    resultado = a + b
+    return resultado
+
+sumatoria = suma(1, 2)
+print(sumatoria)
